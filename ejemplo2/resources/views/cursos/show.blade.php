6b39f2fea6308277aef21eea1b99f92bc1d95ff4
JSanJuan21 <jorgesanjuanfrancisco@gmail.com>
2023-01-19 13:24:22 +0000

    proyecto laravel

---
+++ ejemplo2/resources/views/cursos/show.blade.php
@@ -0,0 +1,7 @@
+    @extends('layout.plantilla')
+
+    @section('title', 'Cursos')
+        
+    @section('content')
+        <h1>Bienvenido al curso de {{$curso}}</h1>
+    @endsection
---
+++ ejemplo2/resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.plantilla')
+
+@section('title', 'home')
+
+@section('content')
+    <h1>Bienvenido a la página principal</h1>
+@endsection
