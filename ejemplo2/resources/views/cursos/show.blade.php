f777b2b4e2e0a728cd9e9e9491a37bb3575d0acc
Jorge San Juan <jorgesanjuanfrancisco@gmail.com>
2023-01-25 19:58:10 +0100

    laravel ejemplo 2

--- ejemplo2/resources/views/cursos/show.blade.php
+++ ejemplo2/resources/views/cursos/show.blade.php
@@ -1,7 +1,10 @@
     @extends('layout.plantilla')
 
-    @section('title', 'Cursos')
+    @section('title', 'Curso ' . $curso->name)
         
     @section('content')
-        <h1>Bienvenido al curso de {{$curso}}</h1>
+        <h1>Bienvenido al curso de {{$curso->name}}</h1>
+        <a href="{{route('cursos.index')}}">Volver a cursos</a>
+        <p><strong>Categoría: </strong>{{$curso->category}}</p>
+        <p>{{$curso->description}}</p>
     @endsection
